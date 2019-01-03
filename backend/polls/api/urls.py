1d57ef2aa3820a1db4f796e97c0d19cd9f55f711
Stinky Pete <mbpeterson19@gmail.com>
2019-01-02 22:23:20 -0700

    backend update/delete generic views

--- backend/polls/api/urls.py
+++ backend/polls/api/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path
+from .views import PollViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import PollListView, PollRetrieveView
+router = DefaultRouter()
+router.register(r'', PollViewSet, basename='polls')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('', PollListView.as_view()),
-    path('<pk>', PollRetrieveView.as_view())
-]
+# from django.urls import path
+
+# from .views import (
+#     PollListView,
+#     PollRetrieveView,
+#     PollCreateView,
+#     PollDestroyView,
+#     PollUpdateView
+# )
+
+# urlpatterns = [
+#     path('', PollListView.as_view()),
+#     path('create/', PollCreateView.as_view()),
+#     path('<pk>', PollRetrieveView.as_view()),
+#     path('<pk>/delete/', PollDestroyView.as_view()),
+#     path('<pk>/update/', PollUpdateView.as_view())
+# ]
--- backend/polls/api/views.py
+++ backend/polls/api/views.py
@@ -1,13 +1,43 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from polls.models import Question
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     DestroyAPIView,
+#     UpdateAPIView
+# )
+# from polls.models import Question
+# from .serializers import PollSerializer
+
+# class PollListView(ListAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = PollSerializer
+
+
+# class PollCreateView(CreateAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = PollSerializer
+
+
+# class PollRetrieveView(RetrieveAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = PollSerializer
+
+
+# class PollDestroyView(DestroyAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = PollSerializer
+
+
+# class PollUpdateView(UpdateAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = PollSerializer
+
+from rest_framework import viewsets
+
 from .serializers import PollSerializer
+from polls.models import Question
 
 
-class PollListView(ListAPIView):
-    queryset = Question.objects.all()
+class PollViewSet(viewsets.ModelViewSet):
     serializer_class = PollSerializer
-
-
-class PollRetrieveView(RetrieveAPIView):
     queryset = Question.objects.all()
-    serializer_class = PollSerializer
